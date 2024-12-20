e2668da2ca149506e13797bdfd9475017a12fbd9
realrnvr <realrnvr@gmail.com>
2024-12-20 23:09:35 +0530

    main updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,16 @@
 import "./resets.css";
 import "./utils.css";
 import "./index.css";
-import { Auth0Provider } from "@auth0/auth0-react";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProviderBase } from "./services/provider/RouterProviderBase";
+import Auth0ProviderBase from "./services/provider/Auth0ProviderBase";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Auth0Provider
-      domain="dev-riaw1h1kzszelhv5.us.auth0.com"
-      clientId="w5dAqh53hZqBrhmIIwytz4Sr9eSF3AR8"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
+    <Auth0ProviderBase>
       <RouterProviderBase />
-    </Auth0Provider>
+    </Auth0ProviderBase>
   </StrictMode>
 );

7c56ca874300d65dadd535686c97c821892e1dc3
realrnvr <realrnvr@gmail.com>
2024-12-21 17:32:34 +0530

    router updates

--- client/src/services/provider/RouterProviderBase.jsx
+++ client/src/services/provider/RouterProviderBase.jsx
@@ -30,7 +30,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/meeting/:callId",
-    element: <Call />,
+    element: (
+      <VideoProvider>
+        <Call />
+      </VideoProvider>
+    ),
   },
 ]);
 

fd40a002ca72198fcbc11f045ef1dbacad8e85a2
realrnvr <realrnvr@gmail.com>
2024-12-25 22:04:33 +0530

    schedule meet addition

---
+++ client/src/pages/schedule/Schedule.jsx
@@ -0,0 +1,12 @@
+import CallList from "@/components/callList/CallList";
+import "./schedule.css";
+
+const Schedule = () => {
+  return (
+    <div>
+      <CallList />
+    </div>
+  );
+};
+
+export default Schedule;

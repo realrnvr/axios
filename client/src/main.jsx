d5c661885b27e025e8e25a1e3db7f909af75b87e
realrnvr <realrnvr@gmail.com>
2024-12-25 21:59:23 +0530

    main files updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,23 @@
 import "./resets.css";
 import "./utils.css";
 import "./index.css";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProviderBase } from "./services/provider/RouterProviderBase";
 import Auth0ProviderBase from "./services/provider/Auth0ProviderBase";
 import { Toaster } from "./components/ui/toaster";
-import "@stream-io/video-react-sdk/dist/css/styles.css";
+import VideoProvider from "./services/stream/VideoProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0ProviderBase>
-      <RouterProviderBase />
-      <Toaster />
+      <VideoProvider>
+        <RouterProviderBase />
+        <Toaster />
+      </VideoProvider>
     </Auth0ProviderBase>
   </StrictMode>
 );

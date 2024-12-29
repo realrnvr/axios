1a147231a796c82afde404986d9ec67f53ff5302
rushichavan21 <rushichavan2327@gmail.com>
2024-12-29 16:21:56 +0530

    newroute added

--- client/src/services/provider/RouterProviderBase.jsx
+++ client/src/services/provider/RouterProviderBase.jsx
@@ -6,6 +6,7 @@ import Call from "@/pages/call/Call";
 import Chat_Layout from "@/pages/chat/Chat";
 import CreateMeet from "@/pages/createMeet/CreateMeet";
 import Upcoming from "@/pages/upcoming/Upcoming";
+import TaskPlanner from "@/components/features/TaskPlanner";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
   {
     path: "/upcoming",
     element: <Upcoming />,
+  },
+  {
+    path: "/taskplanner",
+    element: <TaskPlanner/>,
   },
 ]);
 

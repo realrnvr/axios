bf131da86a41f21aad90fbaab0ec71c7a7730baa
realrnvr <realrnvr@gmail.com>
2025-01-11 00:28:38 +0530

    switch logic fixed

--- client/src/components/callList/CallList.jsx
+++ client/src/components/callList/CallList.jsx
@@ -1,10 +1,11 @@
 import "./call-list.css";
 import { useGetCalls } from "@/hooks/useGetCalls";
-import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
-import CustomLoader from "../customLoader/CustomLoader";
 import { Button } from "../ui/button";
 import { toast } from "@/hooks/use-toast";
+import CustomLoader from "../customLoader/CustomLoader";
+import PropTypes from "prop-types";
+import { Copy } from "lucide-react";
 
 const CallList = ({ type }) => {
   const { endedCalls, upcomingCalls, isLoading } = useGetCalls();
@@ -12,24 +13,22 @@ const CallList = ({ type }) => {
   const navigate = useNavigate();
 
   // select data based on 'type'
-  const getCalls = () => {
+  const getCalls = (type) => {
     switch (type) {
       case "ended":
-        return endedCalls;
-      case "recordings":
-        return upcomingCalls;
+        return endedCalls();
+      case "upcoming":
+        return upcomingCalls();
       default:
         return [];
     }
   };
 
   // fallback message based on 'type'
-  const getNoCallsMessage = () => {
+  const getNoCallsMessage = (type) => {
     switch (type) {
       case "ended":
         return "No Previous Calls";
-      case "recordings":
-        return "No Recordings";
       case "upcoming":
         return "No Upcoming Calls";
       default:
@@ -37,27 +36,57 @@ const CallList = ({ type }) => {
     }
   };
 
-  const calls = getCalls(); // Corrected to use getCalls without parameters
-  const NoCallsMessage = getNoCallsMessage();
+  const calls = getCalls(type); // Corrected to use getCalls without parameters
+  const NoCallsMessage = getNoCallsMessage(type);
 
   if (isLoading) {
     return <CustomLoader />;
   }
 
   return (
-    <article className="call__container">
+    <>
       {calls && calls.length > 0 ? (
         calls.map((meeting, idx) => {
           return (
             <div key={idx} className="meeting__card">
-              <p>id: {meeting?.id}</p>
-              <p>title: {meeting?.state?.custom?.description || "----"}</p>
-              <p>
-                date:{" "}
-                {new Date(meeting?.state?.startsAt).toLocaleString() ||
-                  new Date(meeting?.start_time).toLocaleString()}
-              </p>
               <div>
+                <div className="meeting__top-card">
+                  <label htmlFor="meeting-title" className="meeting__label">
+                    Meet Id:
+                  </label>
+                  <Copy
+                    color="#000"
+                    width={17}
+                    onClick={() => {
+                      navigator.clipboard.writeText(meeting?.id);
+                      toast({ title: "Copied!" });
+                    }}
+                  />
+                </div>
+                <div className="meeting__id">
+                  <p id="meeting-id" className="meeting__id-para">
+                    {meeting?.id}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <label htmlFor="meeting-title" className="meeting__label">
+                  Title:
+                </label>
+                <p id="meeting-title">
+                  {meeting?.state?.custom?.description || "----"}
+                </p>
+              </div>
+              <div>
+                <label htmlFor="meeting-title" className="meeting__label">
+                  Date:
+                </label>
+                <p>
+                  {new Date(meeting?.state?.startsAt).toLocaleString() ||
+                    new Date(meeting?.start_time).toLocaleString()}
+                </p>
+              </div>
+              <div className="meeting__btn-container">
                 {type !== "ended" && (
                   <Button
                     variant="secondary"
@@ -71,17 +100,7 @@ const CallList = ({ type }) => {
                     Copy link
                   </Button>
                 )}
-                {type === "recordings" && (
-                  <Button variant="secondary">Play</Button>
-                )}
-                {type === "recordings" ? (
-                  <Button
-                    variant="secondary"
-                    onClick={() => navigate(`/${meeting?.url}`)}
-                  >
-                    Play
-                  </Button>
-                ) : type === "ended" ? null : (
+                {type === "ended" ? null : (
                   <Button
                     variant="secondary"
                     onClick={() => navigate(`/meeting/${meeting?.id}`)}
@@ -89,13 +108,6 @@ const CallList = ({ type }) => {
                     Start
                   </Button>
                 )}
-                <p>
-                  {type === "recordings"
-                    ? meeting?.url
-                    : type === "ended"
-                    ? null
-                    : `http://localhost:5173/meeting/${meeting?.id}`}
-                </p>
               </div>
             </div>
           );
@@ -103,7 +115,7 @@ const CallList = ({ type }) => {
       ) : (
         <h2>{NoCallsMessage}</h2>
       )}
-    </article>
+    </>
   );
 };
 

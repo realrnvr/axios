d05e9be621f060a11bb84c31709941e1350465f8
rushichavan21 <rushichavan2327@gmail.com>
2025-01-11 13:21:09 +0530

    Added new Atoms

--- client/src/Atoms/Atom.jsx
+++ client/src/Atoms/Atom.jsx
@@ -1,12 +1,33 @@
-import { atom} from "recoil";
-
+import { atom } from "recoil";
 
 export const isHostAtom = atom({
-    key: 'isHost', 
-    default: 'false', 
-  });
+  key: "isHost",
+  default: "false",
+});
 
-  export const isAttendanceActiveAtom = atom({
-    key: 'isAttendanceActive', 
-    default: 'false', 
-  });
+export const isAttendanceActiveAtom = atom({
+  key: "isAttendanceActive",
+  default: "false",
+});
+
+export const languageAtom = atom({
+  key: "languageAtom",
+  default: "cpp",
+});
+
+export const inputAtom = atom({
+  key: "inputAtom",
+  default: "",
+});
+export const codeAtom = atom({
+  key: "codeAtom",
+  default: "",
+});
+export const outputAtom = atom({
+  key: "outputAtom",
+  default: "",
+});
+export const codeErrorAtom = atom({
+  key: "codeError",
+  default: "",
+});

ec9678e9de1e23629210551d2e230bf5ae38dbe4
realrnvr <realrnvr@gmail.com>
2025-01-30 16:01:58 +0530

    prod

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import chatRouter from "./router/chat.js";
 // frontend ports to allow
 app.use(
   cors({
-    origin: ["axios-kohl.vercel.app"],
+    origin: ["https://axios-kohl.vercel.app"],
     credentials: true,
   })
 );
